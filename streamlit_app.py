2950c5f629f1ebc9f2840a7008bfbf232dfea207
NageshMuniraja <32184809+manoHere@users.noreply.github.com>
2023-07-11 16:53:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('Hello streamlit')
 streamlit.header('sample streamlit')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('text streamlit')
 streamlit.text('textA streamlit')
 streamlit.text('textB streamlit')
