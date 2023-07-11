76e63deb39c201a342203cc52b4c7deb71ace057
NageshMuniraja <32184809+manoHere@users.noreply.github.com>
2023-07-11 15:09:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('Hello streamlit')
+streamlit.header('sample streamlit')
+streamlit.text('text streamlit')
+streamlit.text('textA streamlit')
+streamlit.text('textB streamlit')
